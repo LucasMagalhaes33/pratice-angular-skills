29758cd5ea86f76f3589f143ec7063022f915a2c
lucasmagalhaes33 <lucas.magalhaes911@gmail.com>
2023-11-19 00:06:11 -0300

    feature: add mat-spinner and dialog to to courses html.

--- escola-cursos-angular/src/app/courses/courses.module.ts
+++ escola-cursos-angular/src/app/courses/courses.module.ts
@@ -1,9 +1,10 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { CoursesRoutingModule } from './courses-routing.module';
-import { CoursesComponent } from './courses/courses.component';
+import {CoursesRoutingModule} from './courses-routing.module';
+import {CoursesComponent} from './courses/courses.component';
 import {AppMaterialModule} from "../shared/app-material/app-material.module";
+import {SharedModule} from "../shared/shared.module";
 
 
 @NgModule({
@@ -13,7 +14,9 @@ import {AppMaterialModule} from "../shared/app-material/app-material.module";
   imports: [
     CommonModule,
     CoursesRoutingModule,
-    AppMaterialModule
+    AppMaterialModule,
+    SharedModule
   ]
 })
-export class CoursesModule { }
+export class CoursesModule {
+}
--- escola-cursos-angular/src/app/courses/courses/courses.component.ts
+++ escola-cursos-angular/src/app/courses/courses/courses.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import {Course} from "../model/course";
 import {CoursesService} from "../services/courses.service";
 import {catchError, Observable, of} from "rxjs";
+import {MatDialog} from "@angular/material/dialog";
+import {ErrorDialogComponent} from "../../shared/components/error-dialog/error-dialog.component";
 
 @Component({
   selector: 'app-courses',
@@ -13,15 +15,22 @@ export class CoursesComponent {
   displayedColumns = ['name', 'category'];
   loading: any;
 
-  constructor(private coursesService: CoursesService) {
+  constructor(private coursesService: CoursesService,
+              public dialog: MatDialog
+              ) {
     this.courses$ = this.coursesService.list()
       .pipe(
         catchError(error => {
-          console.log(error)
+          this.onError("Erro ao carregar cursos.")
           return of([])
         })
       );
+  }
 
+  onError(errorMsg: string){
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg
+    });
   }
 
 
