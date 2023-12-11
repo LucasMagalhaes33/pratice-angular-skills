c6df5653de7fa32caf38ab50567d7666d476b96c
lucasmagalhaes33 <lucas.magalhaes911@gmail.com>
2023-12-11 17:51:43 -0300

    feat: add logic to save a course.

--- escola-cursos-angular/src/app/courses/course-form/course-form.component.ts
+++ escola-cursos-angular/src/app/courses/course-form/course-form.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {FormBuilder, FormGroup} from "@angular/forms";
 import {CoursesService} from "../services/courses.service";
 import {MatSnackBar} from "@angular/material/snack-bar";
+import {Location} from "@angular/common";
 
 @Component({
   selector: 'app-course-form',
@@ -13,7 +14,8 @@ export class CourseFormComponent {
   form: FormGroup;
   constructor(private formsBuilder: FormBuilder,
               private service: CoursesService,
-              private snackBar: MatSnackBar) {
+              private snackBar: MatSnackBar,
+              private location: Location) {
     this.form = this.formsBuilder.group({
       name: [null],
       category: [null]
@@ -23,17 +25,24 @@ export class CourseFormComponent {
   protected readonly onsubmit = onsubmit;
 
   onCancel() {
-
+    this.location.back();
   }
 
   onSubmit() {
     this.service.save(this.form.value)
-      .subscribe(data => console.log(data),
+      .subscribe(data => this.onSuccess(),
         error => this.onError());
+    this.onCancel();
   }
 
   private onError(){
     this.snackBar.open('Erro ao salvar curso!',
+      '', {duration: 5000});
+  }
+
+  private onSuccess(){
+    this.snackBar.open('Curso salvo com sucesso!',
       '', {duration: 5000})
   }
+
 }
