064c038d40ea3df4cd1106c0524f1b5696e8c3b6
lucasmagalhaes33 <lucas.magalhaes911@gmail.com>
2024-03-19 20:30:16 -0300

    add course-list to projecct

---
+++ escola-cursos-angular/src/app/courses/components/courses-list/courses-list.component.ts
@@ -0,0 +1,40 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+import { Course } from '../../model/course';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
+
+@Component({
+  selector: 'app-courses-list',
+  templateUrl: './courses-list.component.html',
+  styleUrls: ['./courses-list.component.scss'],
+  standalone: false,
+  imports: [MatTableModule, MatIconModule, MatButtonModule]
+})
+export class CoursesListComponent implements OnInit {
+
+  @Input() courses: Course[] = [];
+  @Output() add = new EventEmitter(false);
+  @Output() edit = new EventEmitter(false);
+  @Output() remove = new EventEmitter(false);
+
+  readonly displayedColumns = ['name', 'category', 'actions'];
+
+  constructor() { }
+
+  ngOnInit(): void { }
+
+  onAdd() {
+    this.add.emit(true);
+  }
+
+  onEdit(course: Course) {
+    this.edit.emit(course);
+  }
+
+  onDelete(course: Course) {
+    this.remove.emit(course);
+  }
+
+}
