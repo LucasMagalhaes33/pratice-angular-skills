a7610714ff00e35c3064db6e2df472e5f9995c8c
lucasmagalhaes33 <lucas.magalhaes911@gmail.com>
2024-04-04 19:34:42 -0300

    update angular 17 atualization with standlone components

--- escola-cursos-angular/src/app/courses/containers/courses/courses.component.ts
+++ escola-cursos-angular/src/app/courses/containers/courses/courses.component.ts
@@ -1,54 +1,58 @@
-import {Component} from '@angular/core';
-import {Course} from "../../model/course";
-import {CoursesService} from "../../services/courses.service";
-import {catchError, Observable, of} from "rxjs";
-import {MatDialog} from "@angular/material/dialog";
-import {ErrorDialogComponent} from "../../../shared/components/error-dialog/error-dialog.component";
-import {ActivatedRoute, Router} from "@angular/router";
-import {MatSnackBar} from "@angular/material/snack-bar";
-import {
-  ConfirmationDialogComponent
-} from "../../../shared/components/confirmation-dialog/confirmation-dialog.component";
-import {MatCard} from "@angular/material/card";
-import {MatToolbar} from "@angular/material/toolbar";
-import {CoursesListComponent} from "../../components/courses-list/courses-list.component";
-import {MatProgressSpinner} from "@angular/material/progress-spinner";
-import {AsyncPipe, NgIf} from "@angular/common";
+import { ConfirmationDialogComponent } from './../../../shared/components/confirmation-dialog/confirmation-dialog.component';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+
+import { ErrorDialogComponent } from '../../../shared/components/error-dialog/error-dialog.component';
+import { Course } from '../../model/course';
+import { CoursesService } from '../../services/courses.service';
+import { CoursePage } from '../../model/course-page';
+import { MatPaginator, PageEvent, MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { CoursesListComponent } from '../../components/courses-list/courses-list.component';
+import { AsyncPipe } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.scss'],
-  imports: [
-    MatCard,
-    MatToolbar,
-    CoursesListComponent,
-    MatProgressSpinner,
-    AsyncPipe,
-    NgIf
-  ],
-  standalone: true
+  standalone: true,
+  imports: [MatCardModule, MatToolbarModule, CoursesListComponent, MatPaginatorModule, MatProgressSpinnerModule, AsyncPipe]
 })
-export class CoursesComponent {
-  courses$: Observable<Course[]> | null = null;
-  displayedColumns = ['name', 'category', 'actions'];
-  loading: any;
+export class CoursesComponent implements OnInit {
 
-  constructor(private coursesService: CoursesService,
-              private dialog: MatDialog,
-              private snackBar: MatSnackBar,
-              private router: Router,
-              private route: ActivatedRoute
+  courses$: Observable<CoursePage> | null = null;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  pageIndex = 0;
+  pageSize = 10;
+
+  constructor(
+    private coursesService: CoursesService,
+    public dialog: MatDialog,
+    private router: Router,
+    private route: ActivatedRoute,
+    private snackBar: MatSnackBar
   ) {
     this.refresh();
   }
 
-  refresh() {
-    this.courses$ = this.coursesService.list()
+  refresh(pageEvent: PageEvent = { length: 0, pageIndex: 0, pageSize: 10 }) {
+    this.courses$ = this.coursesService.list(pageEvent.pageIndex, pageEvent.pageSize)
       .pipe(
+        tap(() => {
+          this.pageIndex = pageEvent.pageIndex;
+          this.pageSize = pageEvent.pageSize;
+        }),
         catchError(error => {
-          this.onError("Erro ao carregar cursos.")
-          return of([])
+          this.onError('Erro ao carregar cursos.');
+          return of({ courses: [], totalElements: 0, totalPages: 0 })
         })
       );
   }
@@ -59,17 +63,19 @@ export class CoursesComponent {
     });
   }
 
+  ngOnInit(): void { }
+
   onAdd() {
-    this.router.navigate(['new'], {relativeTo: this.route});
+    this.router.navigate(['new'], { relativeTo: this.route });
   }
 
   onEdit(course: Course) {
-    this.router.navigate(['edit', course._id], {relativeTo: this.route})
+    this.router.navigate(['edit', course._id], { relativeTo: this.route });
   }
 
   onRemove(course: Course) {
     const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
-      data: `Tem certeza que deseja remover esse curso?`,
+      data: 'Tem certeza que deseja remover esse curso?',
     });
 
     dialogRef.afterClosed().subscribe((result: boolean) => {
@@ -77,14 +83,13 @@ export class CoursesComponent {
         this.coursesService.remove(course._id).subscribe(
           () => {
             this.refresh();
-            this.snackBar.open('Curso removido com sucesso!', 'X',
-              {
-                duration: 5000,
-                verticalPosition: 'top',
-                horizontalPosition: 'center'
-              });
+            this.snackBar.open('Curso removido com sucesso!', 'X', {
+              duration: 5000,
+              verticalPosition: 'top',
+              horizontalPosition: 'center'
+            });
           },
-          onerror => this.onError('Erro ao tentar remover curso!')
+          () => this.onError('Erro ao tentar remover curso.')
         );
       }
     });
--- escola-cursos-angular/src/main.ts
+++ escola-cursos-angular/src/main.ts
@@ -1,21 +1,20 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-
-import { importProvidersFrom } from '@angular/core';
-import { AppComponent } from './app/app.component';
-import { withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { AppRoutingModule } from './app/app-routing.module';
-import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {importProvidersFrom} from '@angular/core';
+import {AppComponent} from './app/app.component';
+import {provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {provideAnimations} from '@angular/platform-browser/animations';
+import {bootstrapApplication, BrowserModule} from '@angular/platform-browser';
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
+import {PreloadAllModules, provideRouter, withPreloading} from "@angular/router";
+import {APP_ROUTES} from "./app/app.routes";
 
 
 bootstrapApplication(AppComponent, {
     providers: [
-        importProvidersFrom(BrowserModule, AppRoutingModule, MatToolbarModule),
+        importProvidersFrom(BrowserModule, MatToolbarModule),
         provideAnimations(),
-        provideHttpClient(withInterceptorsFromDi()), provideAnimationsAsync()
+        provideHttpClient(withInterceptorsFromDi()), provideAnimationsAsync(),
+        provideRouter(APP_ROUTES, withPreloading(PreloadAllModules))
     ]
 })
   .catch(err => console.error(err));
